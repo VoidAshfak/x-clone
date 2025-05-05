a19890e554234ed0bcebe91813e928281d4ca78b
VoidAshfak <1703103@student.ruet.ac.bd>
2025-05-05 21:20:43 +0600

    feat: add share and image edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Homepage = () => {
     return <div className="">
-        <div className='px-4 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray'>
+        <div className='px-4 pt-4 flex justify-around text-textGray font-bold border-b-[1px] border-borderGray'>
             <Link className="pb-3 flex items-center border-b-4 border-iconBlue" href="/">For you</Link>
             <Link className="pb-3 flex items-center" href="/">Following</Link>
             <Link className="hidden pb-3 md:flex items-center" href="/">React.js</Link>
